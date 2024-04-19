c9237a92ab1ff18fda1064dd11cc1d0790c574a0
Ahmad Freijeh <ahmadbfreijeh@gmail.com>
2024-04-19 19:26:36 +0300

    Update axios intersptor in repository class:

--- app/repositories/Repository.ts
+++ app/repositories/Repository.ts
@@ -3,56 +3,55 @@ import publicIP from 'react-native-public-ip';
 import DeviceInfo from 'react-native-device-info';
 import {I18nManager, Platform} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {API_URL} from '@env';
+import {API_URL, COUNTRY} from '@env';
 
 const baseURL = `${API_URL}/api`;
 
-const getClientUniqueId = async () => {
-  try {
-    return await DeviceInfo.getUniqueId();
-  } catch (error) {
-    console.error('Failed to get unique device ID:', error);
-    return '';
-  }
+const getDeviceId = () => {
+  DeviceInfo.getUniqueId()
+    .then(id => {
+      return id;
+    })
+    .catch(e => {
+      console.error(e);
+    });
 };
 
-const getPublicIP = async () => {
-  try {
-    return await publicIP();
-  } catch (error) {
-    console.error('Failed to get public IP:', error);
-    return '';
-  }
+const getDeviceIp = () => {
+  publicIP()
+    .then(ip => {
+      return ip;
+    })
+    .catch(e => {
+      console.error(e);
+    });
 };
 
-const fetchClient = async () => {
-  const uniqueId = await getClientUniqueId();
-  const ip = await getPublicIP();
+const fetchClient = () => {
+  const uniqueId = getDeviceId();
+  const ip = getDeviceIp();
+
   const instance = axios.create({
-    baseURL,
+    baseURL: baseURL,
   });
 
   instance.interceptors.request.use(
     async config => {
       config.headers['ip'] = ip;
-      config.headers['country'] = 'LB';
+      config.headers['country'] = COUNTRY ?? '';
       config.headers['os'] = Platform.OS;
       config.headers['device-id'] = uniqueId;
-      config.headers['platform'] = 'customer';
       config.headers['Accept'] = 'application/json';
       config.headers['Content-Type'] =
         config.headers['Content-Type'] || 'application/json';
       config.headers['lang'] = I18nManager.isRTL ? 'ar' : 'en';
       config.headers['version'] = DeviceInfo.getVersion();
 
-      const userJson = await AsyncStorage.getItem('user');
-      if (userJson) {
-        const apiToken = await AsyncStorage.getItem('api_token');
-        if (apiToken) {
-          config.headers['Authorization'] = `Bearer ${apiToken}`;
-        }
+      const user = await AsyncStorage.getItem('user');
+      if (user) {
+        let apiToken = await AsyncStorage.getItem('api_token');
+        config.headers['Authorization'] = `Bearer ${apiToken}`;
       }
-
       return config;
     },
     error => {
